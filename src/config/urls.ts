167265ae8088b2dc54338e92bca482c81a569416
breinard644 <samuel@demanzo.com>
2025-06-30 17:52:14 +0530

    Revert "Fixed bugs"
    This reverts commit 73bd1834ac3d6152d704802ea4625a27d966d88c.

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,2 +1,1 @@
-export const API_URL = 'https://a2.techbuzz360.com/demanzo/wp-json/custom/v1/';
-export const BASE_URL = 'http://localhost:4321/';
+export const API_URL = 'https://demanzo.com/wp-json/';
