73bd1834ac3d6152d704802ea4625a27d966d88c
breinard644 <samuel@demanzo.com>
2025-06-30 17:42:23 +0530

    Fixed bugs

---
+++ src/components/pagesComponents/home/HeroLogger.jsx
@@ -0,0 +1,28 @@
+// src/components/HeroLogger.tsx
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../../config/urls';
+
+export default function HeroSection() {
+
+    const [pageData,setPageData] = useState({})
+  useEffect(() => {
+    axios.get(API_URL+'home-hero')
+      .then((res) => {
+        setPageData(res.data);
+      })
+      .catch((err) => {
+        console.error('Error in browser:', err);
+      });
+  }, []);
+
+  return (
+    <>
+    <div class="flex flex-col justify-center gap-4 py-8 lg:py-16">
+        <p  class="text-[30px] md:text-[40px] font-[600] text-[#191d27]">{pageData.title}</p>
+        <p class="text-[16px] font-[500] text-[#616670]">{pageData.content}</p>
+		<a href={pageData.link} class="bg-[#2d89bf] text-white rounded-full px-6 py-4 md:mt-6 text-[15px] font-[500] w-fit">{pageData.button}</a>
+    </div>
+    </>
+  );
+}
--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,1 +1,2 @@
-export const API_URL = 'https://demanzo.com/wp-json/';
+export const API_URL = 'https://a2.techbuzz360.com/demanzo/wp-json/custom/v1/';
+export const BASE_URL = 'http://localhost:4321/';
