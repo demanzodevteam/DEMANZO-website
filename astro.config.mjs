ce9e288b05d05f372ed30b8a884415e37ac330cd
breinard644 <samuel@demanzo.com>
2025-06-30 13:39:06 +0530

    Home Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import { defineConfig } from 'astro/config';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-        resolve: {
-            alias: {
-                '@': path.resolve('./src'),
-            },
-        },
-    }
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              '@': path.resolve('./src'),
+          },
+      },
+  },
+
+  integrations: [react()]
 });
