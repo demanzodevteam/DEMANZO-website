be9edf91b74b75b77cbd4c19a98ec7ed17600b24
breinard644 <samuel@demanzo.com>
2025-07-01 15:48:48 +0530

    Directory change

--- src/components/pagesComponents/home/Caurocel.jsx
+++ src/components/pagesComponents/home/Caurocel.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import "./styles.css";
+import "../../styles.css";
 
 export default function Caurocel() {
   return (
