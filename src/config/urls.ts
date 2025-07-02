ba8f0c3c1fd5afa766ddbe968189fa4d69902738
breinard644 <samuel@demanzo.com>
2025-07-02 16:50:56 +0530

    Footer Contents Fix

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,4 +1,4 @@
 export const API_URL = 'https://a2.techbuzz360.com/demanzo/wp-json/custom/v1/';
 export const BASE_URL = 'http://localhost:4321/';
 export const FORM_URL = 'https://a2.techbuzz360.com/demanzo/wp-json/contact-form-7/v1/contact-forms/';
-export const NAV_URLS = 'https://a2.techbuzz360.com/demanzo/wp-json/wp/v2/wp_navigation/';
+export const NAV_URLS = 'https://a2.techbuzz360.com/demanzo/wp-json/custom/v1/parsed-navigation/';
