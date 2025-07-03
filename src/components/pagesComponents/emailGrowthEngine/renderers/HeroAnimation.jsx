b896393ecdc073dcc2234a9f6e25e22d75928a14
breinard644 <samuel@demanzo.com>
2025-07-03 18:46:55 +0530

    uri fix

--- src/components/pagesComponents/emailGrowthEngine/renderers/HeroAnimation.jsx
+++ src/components/pagesComponents/emailGrowthEngine/renderers/HeroAnimation.jsx
@@ -1,33 +1,15 @@
-// import { motion } from "framer-motion";
-// import heroImg from "../../../../assets/hero-cropped.webp";
-
-// const HeroAnimation = ({image}) => {
-//   return (
-//     <motion.div
-//       initial={{ opacity: 0, x: -300 }}
-//       whileInView={{ opacity: 1, x: 0 }}
-//       transition={{ duration: 1 }}
-//     >
-//       <img src={image} alt="image" class="w-[500px] md:mx-auto lg:mx-0" />
-//     </motion.div>
-//   );
-// };
-
-// export default HeroAnimation;
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { motion } from "framer-motion";
 import heroImg from "../../../../assets/hero-cropped.webp";
 
-const HeroAnimation = ({ image }) => {
-  useEffect(() => {
-  AOS.init();
-}, []);
-
+const HeroAnimation = ({image}) => {
   return (
-    <div data-aos="fade-right">
-      <img src={image} alt="image" />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, x: -300 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <img src={image} alt="image" class="w-[500px] md:mx-auto lg:mx-0" />
+    </motion.div>
   );
 };
 
