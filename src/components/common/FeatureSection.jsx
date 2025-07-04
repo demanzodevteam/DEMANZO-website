28edb4a78e117fdde8c69125deb905bc6d880a5e
vibindem <vibin.sekhar@demanzo.com>
2025-07-04 15:49:50 +0530

    hero pipeline name changed

---
+++ src/components/common/FeatureSection.jsx
@@ -0,0 +1,52 @@
+// src/components/FeatureSection.jsx
+import React from "react";
+
+const FeatureSection = ({ pageData }) => {
+  const { title = "", cards = [] } = pageData || {};
+
+  return (
+    <section className="bg-[#FFFFFF] px-4 md:px-10 lg:px-20 py-16 space-y-4 text-center">
+      <div className="inline-block relative mx-auto lg:mx-0 mb-20">
+        <div className="relative inline-block">
+          <h1 className="text-[45px] md:text-[48px] lg:text-[45px] font-semibold relative z-10 leading-tight text-center lg:text-left">
+            {title}
+          </h1>
+          {/* SVG below heading */}
+          <svg
+            className="absolute left-1/2 -translate-x-1/2 bottom-[-10px] lg:left-[180px] lg:translate-x-0 z-0"
+            width="270"
+            height="20"
+            viewBox="0 0 270 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5 15C80 5 190 0 265 12"
+              stroke="#7D80E6"
+              strokeWidth="6"
+              strokeLinecap="round"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div className="grid lg:grid-cols-4 gap-6 lg:px-10">
+        {cards.map((card, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-start justify-center p-6 bg-white rounded-lg shadow-md space-y-4"
+          >
+            <div className="flex flex-row items-center justify-center px-3 space-x-2">
+              <div className="text-[#447EFC]">{card.icon}</div>             
+              <h2 className="text-[20px] font-semibold mb-2">{card.title}</h2>
+            </div>
+            <p className="text-start text-[#616670] px-4">{card.description}</p>
+            <hr className="border-t border-[#E0E0E0] w-full mb-6" />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default FeatureSection;
