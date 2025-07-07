1104933d5b8e5898b2b8cd7c0d6b051b27d9324e
breinard644 <samuel@demanzo.com>
2025-07-08 01:46:31 +0530

    Fixed layout issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ const config = {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    container: {
+      center: true,
+      // padding: '1rem',
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1496px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Inter', 'ui-sans-serif', 'system-ui'],
