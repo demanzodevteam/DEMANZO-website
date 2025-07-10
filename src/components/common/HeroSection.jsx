873009d11fed3d3db808aa125203539dc8fffe80
VinithaRam1016 <vinitharambe1016@gmail.com>
2025-07-10 03:01:45 -0700

    bgImgpropchanges
    bgImgpropchanges

--- src/components/common/HeroSection.jsx
+++ src/components/common/HeroSection.jsx
@@ -2,13 +2,13 @@
 import curveImg from "../../assets/Curve-main-bg.webp";
 import { MEDIA_URL } from "../../config/urls";
 
-export default function HeroSection({ pageData ,bgImg }) {
+export default function HeroSection({ pageData }) {
   
   return (
     <>
       <div
         class="grid grid-cols-1 lg:grid-cols-[1fr_0.8fr] px-2 gap-2 lg:px-14 lg:py-10 bg-no-repeat bg-bottom bg-cover min-h-[80vh]"
-        style={{ backgroundImage: bgImg ? "none" : `url('${curveImg.src}')` }}
+        style={{ backgroundImage: pageData?.bgImg ? "none" : `url('${curveImg.src}')` }}
       >
         <div class="flex flex-col justify-center gap-4 py-8 px-4 lg:px-15 lg:py-10">
           <h1 class="text-[#FF5F55] font-semibold text-[18px] lg:text-[20px]">
@@ -46,7 +46,7 @@ export default function HeroSection({ pageData ,bgImg }) {
         </div>
 
         <div class="flex items-center justify-center pt-8 pb-12 lg:pt-0 lg:pb-0 ">
-          {bgImg ? (
+          {pageData?.bgImg ? (
             <>
               <div class="relative w-full max-w-md">
                 <div class="absolute inset-0 h-[300px] bg-gray-100 transform translate-x-10 -translate-y-12 rounded-[30px] shadow-lg"></div>
