5a3b588890a74cef24c5a86043cb4f6164d7ebb1
breinard644 <samuel@demanzo.com>
2025-07-11 17:40:28 +0530

    Seo iot page completed

--- src/components/common/HeadingBanner.jsx
+++ src/components/common/HeadingBanner.jsx
@@ -7,8 +7,8 @@ const HeadingBanner = ({ pageData }) => {
     <div className="bg-[#e6f8ff] px-[15px] py-[90px] relative">
       <div className="relative mx-auto lg:mx-0 text-center flex items-center justify-center">
         <h1 className="text-[45px] md:text-[50px] font-semibold relative z-10">
-          {pageData.headings[0]}<br></br>
-          {pageData.headings[1]}
+          {pageData?.headings[0]}<br></br>
+          {pageData?.headings[1]}
         </h1>
         <svg
           className="mt-2 mx-auto w-[270px] h-[20px] absolute bottom-0"
--- src/components/common/NeedHighlight.jsx
+++ src/components/common/NeedHighlight.jsx
@@ -8,54 +8,62 @@ const NeedHighlight = ({ SectionData }) => {
       <div className="inline-block relative mx-auto lg:mx-0 mb-10">
         <div className="relative inline-block">
           <h1 className="text-[45px] md:text-[48px] lg:text-[45px] font-semibold relative z-10 leading-tight text-center lg:text-center">
-            {SectionData.headings?.[0]}
+            {SectionData?.headings?.[0]}
           </h1>
         </div>
       </div>
 
-      <p className="mt-12 text-gray-600 font-medium max-w-7xl mx-auto text-[16px] leading-relaxed">
-        {SectionData.paragraphs[0]}
-      </p>
+      {
+        SectionData?.paragraphs[0] != '' && (
+          <p className="mt-12 text-gray-600 font-medium max-w-7xl mx-auto text-[16px] leading-relaxed">
+            {SectionData?.paragraphs[0]}
+          </p>
+        )
+      }
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:px-30 mt-10">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:px-30 mt-10 items-stretch">
         {SectionData?.list_items[0]?.map((card, index) => {
           const isOdd = index % 2 === 0;
           return (
             <div
               key={index}
-              className={`group shadow-md rounded-xl px-8 py-10 flex flex-col items-center space-y-4 transition max-w-[360px] mx-auto
-          ${
-            isOdd
-              ? "bg-white text-black hover:bg-[#060A33] hover:text-white"
-              : "bg-[#060A33] text-white"
-          }`}
+              className={`group shadow-md rounded-xl px-8 py-10 flex flex-col justify-between items-center space-y-4 transition max-w-[360px] w-full h-full mx-auto
+        ${isOdd
+                  ? "bg-white text-black hover:bg-[#060A33] hover:text-white"
+                  : "bg-[#060A33] text-white"
+                }`}
             >
               <div
                 className={`w-14 h-14 transition 
-            ${
-              isOdd
-                ? "text-[#0A6AAD] group-hover:text-white"
-                : "text-[#0A6AAD] group-hover:text-white"
-            }`}
-                dangerouslySetInnerHTML={{ __html: card.svg }}
+          ${isOdd
+                    ? "text-[#0A6AAD] group-hover:text-white"
+                    : "text-[#0A6AAD] group-hover:text-white"
+                  }`}
+                dangerouslySetInnerHTML={{ __html: card?.svg || "" }}
               />
               <p
                 className="font-medium text-[16px] text-center transition"
-                dangerouslySetInnerHTML={{ __html: card.para }}
+                dangerouslySetInnerHTML={{ __html: card?.para || "<span>&nbsp;</span>" }}
               />
             </div>
           );
         })}
       </div>
 
-      <div className="mt-12 flex justify-center">
-        <button
-          type="submit"
-          className="w-full lg:w-auto text-white bg-[#0A6AAD] px-6 py-3 rounded-full mt-8"
-        >
-          {SectionData.paragraphs[1]}
-        </button>
-      </div>
+        <div className="mt-12 flex justify-center pb-20">
+      {SectionData?.paragraphs[1] && SectionData.paragraphs[1].trim() !== "" && (
+
+          <button
+            type="submit"
+            className="w-full lg:w-auto text-white bg-[#0A6AAD] px-6 py-3 rounded-full mt-8"
+          >
+            {SectionData.paragraphs[1]}
+          </button>
+      )}
+
+        </div>
+
     </section>
   );
 };
