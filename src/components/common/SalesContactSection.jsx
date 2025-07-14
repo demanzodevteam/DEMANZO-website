025c7fd0a40621f7cc7f67bc654ecca0f48e840b
breinard644 <samuel@demanzo.com>
2025-07-14 15:25:56 +0530

    Request Demo Form

--- src/components/common/SalesContactSection.jsx
+++ src/components/common/SalesContactSection.jsx
@@ -1,6 +1,7 @@
 import CaptchaImg from '../../assets/Captcha.png';
 import BackGroundImg from '../../assets/Background-SEO-Traffic.jpg';
 import rocketImg from '../../assets/rocket.png';
+import RequestDemoForm from './RequestDemoForm';
 import skyImg from '../../assets/sky2.png'
 
 const SalesContactSection = ({ SectionData }) => {
@@ -36,90 +37,8 @@ const SalesContactSection = ({ SectionData }) => {
         </div>
 
         {/* Right Side - Form */}
-        <div className="bg-white rounded-2xl shadow-lg p-6 lg:p-10">
-          <form className="space-y-6">
-            {/* Name & Email */}
-            <div className="grid grid-cols-2 gap-6">
-              <input
-                type="text"
-                placeholder="Your Name"
-                required
-                className="w-full border border-[#e7e7e7] hover:border-black focus:border-black focus:outline-none px-4 py-3 text-[16px] text-[#050607]"
-              />
-              <input
-                type="email"
-                placeholder="Your Email"
-                required
-                className="w-full border border-[#e7e7e7] hover:border-black focus:border-black focus:outline-none px-4 py-3 text-[16px] text-[#050607]"
-              />
-            </div>
-
-            {/* Number & Website */}
-            <div className="grid grid-cols-2 gap-6">
-              <input
-                type="number"
-                placeholder="Your Number"
-                required
-                className="w-full border border-[#e7e7e7] hover:border-black focus:border-black focus:outline-none px-4 py-3 text-[16px] text-[#050607]"
-              />
-              <input
-                type="url"
-                placeholder="Your Website"
-                required
-                className="w-full border border-[#e7e7e7] hover:border-black focus:border-black focus:outline-none px-4 py-3 text-[16px] text-[#050607]"
-              />
-            </div>
-
-            {/* Dropdown */}
-            <div>
-              <label
-                htmlFor="messageType"
-                className="block mb-1 text-[#050607] text-[16px]"
-              >
-                Reason To Contact
-              </label>
-              <select
-                id="messageType"
-                defaultValue=""
-                className="w-full border border-[#e7e7e7] hover:border-black focus:border-black focus:outline-none px-4 py-3 text-[16px] text-[#050607]"
-              >
-                <option value="" disabled>
-                  I'm interested in
-                </option>
-                <option value="inquiry">Digital Marketing</option>
-                <option value="support">Social Media Management</option>
-                <option value="feedback">Website Development</option>
-                <option value="design">Graphic Design</option>
-                <option value="white-label">
-                  White Label Digital Marketing
-                </option>
-              </select>
-            </div>
-
-            {/* Message */}
-            <textarea
-              rows="4"
-              placeholder="Your Message"
-              className="w-full border border-[#e7e7e7] hover:border-black focus:border-black focus:outline-none px-4 py-3 text-[16px] text-[#050607] resize-none"
-            ></textarea>
-
-            {/* Extra Text Input */}
-            <input
-              type="text"
-              placeholder="Anything else?"
-              className="w-full border border-[#e7e7e7] hover:border-black focus:border-black focus:outline-none px-4 py-3 text-[16px] text-[#050607]"
-            />
-
-            {/* Submit Button */}
-            <div className="flex flex-col items-center gap-4">
-              <button
-                type="submit"
-                className="w-full lg:w-auto text-white bg-[#0A6AAD] px-6 py-3 rounded-full hover:bg-[#f5a31c] transition"
-              >
-                Request a demo
-              </button>
-            </div>
-          </form>
+        <div className=" rounded-2xl shadow-lg p-6 lg:p-10 w-full">
+                  <RequestDemoForm/>
         </div>
       </div>
     </div>
