10f5bb51cb3990efc45d458fdadcd2a551807a15
breinard644 <samuel@demanzo.com>
2025-07-15 15:03:51 +0530

    fixed design issue on hero section

--- src/components/common/HeroSection.jsx
+++ src/components/common/HeroSection.jsx
@@ -7,12 +7,13 @@ export default function HeroSection({ pageData }) {
   return (
     <>
       <div
-        className="grid grid-cols-1 lg:grid-cols-[1fr_0.8fr] px-2 gap-2 lg:px-14 lg:py-10 bg-no-repeat bg-bottom bg-cover min-h-[80vh]"
+        className="demanzo-container"
         style={{
           backgroundImage: pageData?.bgImg ? "none" : `url('${curveImg.src}')`,
         }}
       >
-        <div className="flex flex-col justify-center gap-4 py-8 px-4 lg:px-15 lg:py-10 text-center md:text-left">
+        <div className="demanzo-max-screen-width demanzo-grid">
+<div className="flex flex-col justify-center gap-4 py-8 px-4 lg:px-15 lg:py-10 text-center md:text-left">
           <h1 className="text-[#FF5F55] font-semibold text-[18px] lg:text-[20px]">
             {pageData.title}
           </h1>
@@ -124,6 +125,8 @@ export default function HeroSection({ pageData }) {
             ))}
           </div>
         </div>
+        </div>
+        
       </div>
     </>
   );
