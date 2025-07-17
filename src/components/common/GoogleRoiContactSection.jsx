2d59cfc56f45efab471a4f3eebef4950845fdd00
irfanamd14 <irfan.ahamed@demanzo.com>
2025-07-17 19:17:13 +0530

    Google Ads Roi Added

--- src/components/common/GoogleRoiContactSection.jsx
+++ src/components/common/GoogleRoiContactSection.jsx
@@ -1,38 +1,54 @@
 import CaptchaImg from '../../assets/Captcha.png';
 import BackGroundImg from '../../assets/Background-SEO-Traffic.jpg';
 import rocketImg from '../../assets/rocket.png';
-import RequestDemoForm from './RequestDemoForm';
-import skyImg from '../../assets/sky2.png'
+import skyImg from '../../assets/sky2.png';
+import { motion } from "framer-motion";
 import GoogleRoiContactForm from './GoogleRoiContactForm';
 
 const SalesContactSection = ({ SectionData }) => {
 
   return (
     <div
-      className="flex items-center justify-center lg:px-4 py-16 bg-cover bg-center" id="contact"
-      style={{ backgroundImage: `url('${BackGroundImg.src}')` }}
+      className="flex items-center justify-center lg:px-4 bg-cover bg-center"
+      style={{ backgroundImage: `url(${BackGroundImg.src})` }}
     >
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-7xl w-full overflow-hidden p-8 lg:p-14">
         {/* Left Side */}
         <div className="flex flex-col items-center justify-center text-center gap-6 px-4">
-          <img
-            src={rocketImg.src}
-            alt="Rocket"
-            className="hover:-translate-y-4 transition-all duration-500"
-          />
+          <motion.img
+  src={rocketImg.src}
+  alt="Rocket"
+  className="w-auto h-auto"
+  animate={{
+    y: [0, -10, 0], // up, then back down
+  }}
+  transition={{
+    duration: 1.5,
+    repeat: Infinity,
+    ease: "easeInOut",
+  }}
+/>
           <h2 className="demanzo-form-heading">{SectionData?.headings}</h2>
 
           {SectionData?.list_items[0].map((item, index) => (
-            <p key={index} className="demanzo-form-para-text">
+            <p key={index} className="demanzo-form-para-text !text-white">
               {item.para}
             </p>
           ))}
 
-          <img
-            src={skyImg.src}
-            alt="Sky"
-            className="hover:-translate-y-4 transition-all duration-500 mt-6"
-          />
+          <motion.img
+  src={skyImg.src}
+  alt="Sky"
+  className="mt-6 w-auto h-auto"
+  animate={{
+    y: [0, -8, 0], // subtle float
+  }}
+  transition={{
+    duration: 2,
+    repeat: Infinity,
+    ease: "easeInOut",
+  }}
+/>
         </div>
 
         {/* Right Side - Form */}
