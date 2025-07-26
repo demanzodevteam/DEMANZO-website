e9e543f77c5ae53c7b00dbcbc3c50c22d976d66c
VinithaRam1016 <vinitharambe1016@gmail.com>
2025-07-26 18:36:01 +0530

    casestudybanner

--- src/components/common/DynamicBanner.jsx
+++ src/components/common/DynamicBanner.jsx
@@ -3,12 +3,13 @@ import { MEDIA_URL } from "../../config/urls";
 
 
 const DynamicBanner = ({ pageData }) => {
+  // console.log(pageData,'pageData list');
   
   return (
     <div className="bg-[#e6f8ff] px-[15px] py-[90px] relative">
       <div className="relative mx-auto lg:mx-0 text-center flex items-center justify-center">
         <h1 className="demanzo-h1 ">
-          {pageData.name}<br></br>
+          {pageData[0].title}<br></br>
         </h1>
         <svg
           className="mt-2 mx-auto w-[270px] h-[20px] absolute bottom-0"
