e340202fc3ebfe282b5ff1b867119c70c1a9e207
breinard644 <samuel@demanzo.com>
2025-07-28 17:19:29 +0530

    blog fix

--- src/components/common/DynamicBanner.jsx
+++ src/components/common/DynamicBanner.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { MEDIA_URL } from "../../config/urls";
 
 
 const DynamicBanner = ({ pageData }) => {
-  
+   
   return (
     <div className="bg-[#e6f8ff] px-[15px] py-[90px] relative">
       <div className="relative mx-auto lg:mx-0 text-center flex items-center justify-center">
         <h1 className="demanzo-h1 ">
-          {pageData[0].title}<br></br>
+          {pageData?.name}<br></br>
         </h1>
         <svg
           className="mt-2 mx-auto w-[270px] h-[20px] absolute bottom-0"
